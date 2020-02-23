c3326d802879af617153e00ce0831014c0d320f1
marvinboris <47755699+marvinboris@users.noreply.github.com>
2020-02-23 20:38:31 +0100

    Updated verification mail

--- resources/views/emails/verification-link.blade.php
+++ resources/views/emails/verification-link.blade.php
@@ -1,16 +1,44 @@
-<div style="margin: auto; width: 40%; min-width: 300px; max-width: 100%; font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;">
-    <h4>Hello !</h4>
-    <p>
-        Please click the button below to verify your email address.
-    </p>
-    <p style="text-align: center;">
-        <a href="{{ $link }}" style="border: none; background: #005eb1; font-size: small; padding: .5rem 1rem; text-decoration: none; color: white;">Verify E-Mail Address</a>
-    </p>
-    <p>
-        If you did not create an account, no further action is required.
-    </p>
-    <p>
-        Regards,<br>
-        Trading Academy
-    </p>
+<div
+    style="background-color: white; width: 100%; font-size: 1.2rem; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;">
+    <div style="padding: 3rem 5%; background-color: forestgreen; color: white;">
+        <div style="display: flex; justify-content: space-between; align-items: center;">
+            <div style="display: flex; align-items: center;">
+                <div style="padding-right: 2rem;">
+                    <div style="background-color: white; width: 1rem; height: 6rem;"></div>
+                </div>
+                <div style="font-size: xx-large;">
+                    <div><span style="font-weight: 300;">Thank you for choosing</span></div>
+                    <div><strong>GIT Academy</strong> !</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div style="padding-top: 3rem; padding-bottom: 3rem; margin: auto; width: 80%;">
+        <h1 style="color: forestgreen;">YOUR ACCOUNT WAS CREATED SUCCESSFULLY</h1>
+        <p style="width: 60%; min-width: 19rem; font-weight: 300;">
+            Greetings from <span style="font-weight: 700;">Global Investment Trading Academy</span>. <br>
+            We thank you very much for your concern in our Crypto Trading Academy.<br>
+            Your account is not yet activated. Please find the activation button and click
+            on it to activate your account. We strongly recommend you to read the
+            attached terms and conditions before activating your account.
+        </p>
+        <p>
+            <strong>
+                Kind Regards <br>
+                The support.
+            </strong><br><br>
+        </p>
+        <p style="font-size: x-large;">
+            <a href="#"
+                style="display: inline-block; margin-right: 1rem; margin-bottom: 1rem; border: none; border-radius: 3px; background: orangered; padding: .75rem 2rem; text-decoration: none; color: white;">Terms
+                & Conditions</a>
+            <a href="{{ $link }}"
+                style="display: inline-block; margin-right: 1rem; margin-bottom: 1rem; border: none; border-radius: 3px; background: forestgreen; padding: .75rem 2rem; text-decoration: none; color: white;">Activate
+                account</a>
+        </p>
+        <br>
+        <p style="text-align: center; color: red;">
+            <strong>Do not reply to this Email.</strong>
+        </p>
+    </div>
 </div>
