7bc6bcc788ec10ed5e55025e81349012d7343bca
marvinboris <47755699+marvinboris@users.noreply.github.com>
2020-03-03 20:59:32 +0100

    Fixed mobile dashboard sidebar

--- resources/views/includes/auth/admin/aside.blade.php
+++ resources/views/includes/auth/admin/aside.blade.php
@@ -25,48 +25,7 @@
             <div class="text-700 pt-2 text-large text-montserrat-alt">{{ Auth::guard('admin')->user()->name }}</div>
         </div>
         <div class="flex-fill text-white text-montserrat border-top border-white-20 pt-4">
-            @component('components.auth.aside.admin.link', ['link' => route('admin.dashboard'), 'icon' => 'fas fa-tachometer-alt']) Dashboard @endcomponent
-            @component('components.auth.aside.admin.dropdown', ['name' => 'admin', 'icon' => 'fas fa-user-cog', 'title' => 'Admins', 'link' => route('admin.admins.index')])
-                @component('components.auth.aside.admin.dropdown-item', ['name' => 'Admins List', 'link' => route('admin.admins.index')]) @endcomponent
-                @component('components.auth.aside.admin.dropdown-item', ['name' => 'Add an Admin', 'link' => route('admin.admins.create')]) @endcomponent
-            @endcomponent
-            @component('components.auth.aside.admin.dropdown', ['name' => 'author', 'icon' => 'fas fa-user-tag', 'title' => 'Authors', 'link' => route('admin.authors.index')])
-                @component('components.auth.aside.admin.dropdown-item', ['name' => 'Authors List', 'link' => route('admin.authors.index')]) @endcomponent
-                @component('components.auth.aside.admin.dropdown-item', ['name' => 'Add an Author', 'link' => route('admin.authors.create')]) @endcomponent
-            @endcomponent
-            @component('components.auth.aside.admin.dropdown', ['name' => 'teacher', 'icon' => 'fas fa-chalkboard-teacher', 'title' => 'Teachers', 'link' => route('admin.teachers.index')])
-                @component('components.auth.aside.admin.dropdown-item', ['name' => 'Teachers List', 'link' => route('admin.teachers.index')]) @endcomponent
-                @component('components.auth.aside.admin.dropdown-item', ['name' => 'Add a Teacher', 'link' => route('admin.teachers.create')]) @endcomponent
-            @endcomponent
-            @component('components.auth.aside.admin.dropdown', ['name' => 'student', 'icon' => 'fas fa-user-graduate', 'title' => 'Students', 'link' => route('admin.students.index')])
-                @component('components.auth.aside.admin.dropdown-item', ['name' => 'Students List', 'link' => route('admin.students.index')]) @endcomponent
-                @component('components.auth.aside.admin.dropdown-item', ['name' => 'Add a Student', 'link' => route('admin.students.create')]) @endcomponent
-            @endcomponent
-            @component('components.auth.aside.admin.dropdown', ['name' => 'photo', 'icon' => 'fas fa-file-image', 'title' => 'Photos', 'link' => route('admin.photos.index')])
-                @component('components.auth.aside.admin.dropdown-item', ['name' => 'Photos List', 'link' => route('admin.photos.index')]) @endcomponent
-                @component('components.auth.aside.admin.dropdown-item', ['name' => 'Add a Photo', 'link' => route('admin.photos.create')]) @endcomponent
-            @endcomponent
-            @component('components.auth.aside.admin.dropdown', ['name' => 'document', 'icon' => 'fas fa-file', 'title' => 'Documents', 'link' => route('admin.documents.index')])
-                @component('components.auth.aside.admin.dropdown-item', ['name' => 'Documents List', 'link' => route('admin.documents.index')]) @endcomponent
-                @component('components.auth.aside.admin.dropdown-item', ['name' => 'Add a Document', 'link' => route('admin.documents.create')]) @endcomponent
-            @endcomponent
-            @component('components.auth.aside.admin.dropdown', ['name' => 'course', 'icon' => 'fas fa-school', 'title' => 'Courses', 'link' => route('admin.courses.index')])
-                @component('components.auth.aside.admin.dropdown-item', ['name' => 'Courses List', 'link' => route('admin.courses.index')]) @endcomponent
-                @component('components.auth.aside.admin.dropdown-item', ['name' => 'Add a Course', 'link' => route('admin.courses.create')]) @endcomponent
-            @endcomponent
-            @component('components.auth.aside.admin.dropdown', ['name' => 'session', 'icon' => 'fas fa-clock', 'title' => 'Sessions', 'link' => route('admin.sessions.index')])
-                @component('components.auth.aside.admin.dropdown-item', ['name' => 'Sessions List', 'link' => route('admin.sessions.index')]) @endcomponent
-                @component('components.auth.aside.admin.dropdown-item', ['name' => 'Add a Session', 'link' => route('admin.sessions.create')]) @endcomponent
-            @endcomponent
-            @component('components.auth.aside.admin.dropdown', ['name' => 'deposit', 'icon' => 'fas fa-dollar-sign', 'title' => 'Deposits', 'link' => route('admin.deposits.index')])
-                @component('components.auth.aside.admin.dropdown-item', ['name' => 'Deposits List', 'link' => route('admin.deposits.index')]) @endcomponent
-                @component('components.auth.aside.admin.dropdown-item', ['name' => 'Add a Deposit', 'link' => route('admin.deposits.create')]) @endcomponent
-            @endcomponent
-            @component('components.auth.aside.admin.link', ['link' => url('admin/notifications'), 'icon' => 'fas fa-bell']) Notifications @endcomponent
-            @component('components.auth.aside.admin.dropdown', ['name' => 'settings', 'icon' => 'fas fa-cog', 'title' => 'Settings', 'link' => url('admin/settings')])
-                @component('components.auth.aside.admin.dropdown-item', ['name' => 'Change password', 'link' => '#']) @endcomponent
-                @component('components.auth.aside.admin.dropdown-item', ['name' => 'Edit language', 'link' => '#']) @endcomponent
-            @endcomponent
+            @include('includes.auth.aside-content')
         </div>
     </div>
 </aside>
---
+++ resources/views/includes/auth/user/aside-content.blade.php
@@ -0,0 +1,49 @@
+@component('components.auth.aside.link', ['link' => route(strtolower(Auth::user()->role->name) . '.dashboard'), 'icon' => 'fas fa-tachometer-alt']) Dashboard @endcomponent
+@if (Auth::user()->role->name === 'Author')
+    @component('components.auth.aside.dropdown', ['name' => 'blog', 'icon' => 'fas fa-blog', 'title' => 'Blog', 'link' => route('author.posts.index')])
+        @component('components.auth.aside.dropdown-item', ['name' => 'My Posts', 'link' => route('author.posts.index')]) @endcomponent
+        @component('components.auth.aside.dropdown-item', ['name' => 'Add a Post', 'link' => route('author.posts.create')]) @endcomponent
+    @endcomponent
+@endif
+@if (Auth::user()->role->name === 'Teacher')
+    @component('components.auth.aside.dropdown', ['name' => 'courses', 'icon' => 'fas fa-book-open', 'title' => 'Courses', 'link' => route('teacher.courses.index')])
+        @component('components.auth.aside.dropdown-item', ['name' => 'My Courses', 'link' => route('teacher.courses.index')]) @endcomponent
+        @component('components.auth.aside.dropdown-item', ['name' => 'Add a Course', 'link' => route('teacher.courses.create')]) @endcomponent
+    @endcomponent
+    @component('components.auth.aside.dropdown', ['name' => 'sessions', 'icon' => 'fas fa-clock', 'title' => 'Sessions', 'link' => route('teacher.sessions.index')])
+        @component('components.auth.aside.dropdown-item', ['name' => 'My Sessions', 'link' => route('teacher.sessions.index')]) @endcomponent
+        @component('components.auth.aside.dropdown-item', ['name' => 'Add a Session', 'link' => route('teacher.sessions.create')]) @endcomponent
+    @endcomponent
+    @component('components.auth.aside.dropdown', ['name' => 'documents', 'icon' => 'fas fa-file', 'title' => 'Documents', 'link' => route('teacher.documents.index')])
+        @component('components.auth.aside.dropdown-item', ['name' => 'My Documents', 'link' => route('teacher.documents.index')]) @endcomponent
+        @component('components.auth.aside.dropdown-item', ['name' => 'Add a Document', 'link' => route('teacher.documents.create')]) @endcomponent
+    @endcomponent
+    @component('components.auth.aside.dropdown', ['name' => 'sms', 'icon' => 'fas fa-at', 'title' => 'SMS', 'link' => route('teacher.messages.index')])
+        @component('components.auth.aside.dropdown-item', ['name' => 'SMS List', 'link' => route('teacher.messages.index')]) @endcomponent
+        @component('components.auth.aside.dropdown-item', ['name' => 'Send SMS', 'link' => route('teacher.messages.create')]) @endcomponent
+    @endcomponent
+@endif
+@if (Auth::user()->role->name === 'Student')
+    @component('components.auth.aside.dropdown', ['name' => 'courses', 'icon' => 'fas fa-book', 'title' => 'Courses', 'link' => route('student.courses.index')])
+        @component('components.auth.aside.dropdown-item', ['name' => 'My Courses', 'link' => route('student.courses.mine')]) @endcomponent
+        @component('components.auth.aside.dropdown-item', ['name' => 'Courses List', 'link' => route('student.courses.index')]) @endcomponent
+    @endcomponent
+@endif
+@component('components.auth.aside.dropdown', ['name' => 'finance', 'icon' => 'fas fa-exchange-alt', 'title' => 'Finance', 'link' => url('user/finance')])
+    @component('components.auth.aside.dropdown-item', ['name' => 'Transfer', 'link' => route('user.finance.transfers.create')]) @endcomponent
+    @component('components.auth.aside.dropdown-item', ['name' => 'Transfer List', 'link' => route('user.finance.transfers.index')]) @endcomponent
+    @component('components.auth.aside.dropdown-item', ['name' => 'Add Deposit', 'link' => route('user.finance.deposits.create')]) @endcomponent
+    @component('components.auth.aside.dropdown-item', ['name' => 'Deposit List', 'link' => route('user.finance.deposits.index')]) @endcomponent
+    @component('components.auth.aside.dropdown-item', ['name' => 'Withdraw', 'link' => route('user.finance.withdraws.create')]) @endcomponent
+    @component('components.auth.aside.dropdown-item', ['name' => 'Withdraw List', 'link' => route('user.finance.withdraws.index')]) @endcomponent
+@endcomponent
+@component('components.auth.aside.link', ['link' => route('user.team'), 'icon' => 'fas fa-users']) Team @endcomponent
+@component('components.auth.aside.link', ['link' => route('user.messages'), 'icon' => 'fas fa-envelope']) Messages @endcomponent
+@component('components.auth.aside.link', ['link' => route('user.notifications'), 'icon' => 'fas fa-bell']) Notifications @endcomponent
+@component('components.auth.aside.dropdown', ['name' => 'settings', 'icon' => 'fas fa-cog', 'title' => 'Settings', 'link' => url('user/settings')])
+    @component('components.auth.aside.dropdown-item', ['name' => 'Change password', 'link' => '#']) @endcomponent
+    @if (!Auth::user()->is_verified)
+    @component('components.auth.aside.dropdown-item', ['name' => 'Verify my account', 'link' => '#']) @endcomponent
+    @endif
+    @component('components.auth.aside.dropdown-item', ['name' => 'Edit language', 'link' => '#']) @endcomponent
+@endcomponent
