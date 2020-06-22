846bdd6dfde05768ded6830923e1fd66141cdecb
marvinboris <47755699+marvinboris@users.noreply.github.com>
2020-06-22 18:03:04 +0100

    Google Ads

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -35,4 +35,15 @@
         <title>Trading Academy</title>
     </head>
     <body class="min-vh-100">
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <!-- Bloc d'annonce test -->
+        <ins class="adsbygoogle"
+            style="display:block"
+            data-ad-client="ca-pub-1564976955320089"
+            data-ad-slot="2999023345"
+            data-ad-format="auto"
+            data-full-width-responsive="true"></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
         <div id="app">
