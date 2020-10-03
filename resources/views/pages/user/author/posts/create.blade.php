7399f017afca5c709a6f038cffb98d1c800edf99
marvinboris <47755699+marvinboris@users.noreply.github.com>
2020-10-03 12:48:57 +0100

    Admin cancel buttons

--- resources/views/pages/user/author/posts/create.blade.php
+++ resources/views/pages/user/author/posts/create.blade.php
@@ -15,7 +15,7 @@
     </div>
     <div class="text-center text-montserrat px-3">
         <div><button class="btn btn-block btn-green font-weight-bold mb-3">Save</button></div>
-        <div><a href="route" class="btn btn-block btn-orange font-weight-bold">Cancel</a></div>
+        <div><a href="{{ route('author.posts.index') }}" class="btn btn-block btn-orange font-weight-bold">Cancel</a></div>
     </div>
 </div>
 @endcomponent
